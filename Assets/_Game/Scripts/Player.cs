e0adf3f3d5cec32ac530362fae7dc4ceb7b163f2
Cameron <cameo221@gmail.com>
2025-04-04 22:50:10 -0700

    added light to mouse and volume

--- Assets/_Game/Scripts/Player.cs
+++ Assets/_Game/Scripts/Player.cs
@@ -1,5 +1,6 @@
 using CamLib;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 using UnityEngine.SceneManagement;
 
 public class Player : Singleton<Player>
@@ -8,6 +9,7 @@ public class Player : Singleton<Player>
 
     public float moveSpeed;
     public Rigidbody2D _rb;
+    public Light2D flashlight;
 
     private Vector2 MoveInput;
     
@@ -18,20 +20,39 @@ public class Player : Singleton<Player>
 
     private void Update()
     {
+        DoMoveInput();
+        TryCheats();
+        UpdateFlashlight();
+    }
+
+    private void DoMoveInput()
+    {
         //translate player
         float horiz = Input.GetAxisRaw("Horizontal");
         float vert = Input.GetAxisRaw("Vertical");
         Vector3 move = new Vector3(horiz, vert, 0).normalized;
         MoveInput = move;
         _rb.linearVelocity = MoveInput * (moveSpeed);
+    }
 
-
+    private void TryCheats()
+    {
         if (Debug.isDebugBuild && Input.GetMouseButtonDown(2))
         {
             transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         }
     }
-    
+
+    private void UpdateFlashlight()
+    {
+        //direction to mouse
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 flashlightPos = Camera.main.transform.position;
+        Vector2 dir =  mousePos - flashlightPos;
+        
+        flashlight.transform.up = dir;
+    }
+
     private void FixedUpdate()
     {
     }
