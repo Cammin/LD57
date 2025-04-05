d92b71dfd651ea8dec0c578281e3cc370c12480c
brianpimentel <brianepimentel@gmail.com>
2025-04-05 00:19:28 -0400

    More ghost stuff

--- Assets/_Game/Scripts/Player.cs
+++ Assets/_Game/Scripts/Player.cs
@@ -1,7 +1,7 @@
 using CamLib;
 using UnityEngine;
 
-public class Player : Singleton<MonoBehaviour>
+public class Player : Singleton<Player>
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
