da690b274d8b9c4941b765e63c077ee0d0a511a5
Cameron <cameo221@gmail.com>
2025-04-05 15:14:39 -0700

    some level stuff and bug fixes

--- Assets/_Game/Scripts/Ghosts/_GhostBase.cs
+++ Assets/_Game/Scripts/Ghosts/_GhostBase.cs
@@ -70,7 +70,7 @@ public sealed class _GhostBase : MonoBehaviour
             if (Vector2.Distance(Player.Instance.transform.position, transform.position) <= AttackPlayerRange)
             {
                 CooldownRemaining = ActionCooldownDuration;
-                GhostBehaviour.GhostAction();
+                if (GhostBehaviour) GhostBehaviour.GhostAction();
             }
         }
     }
