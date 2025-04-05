285c7e3e6cc70590e1c709693fd0526691d04888
Cameron <cameo221@gmail.com>
2025-04-04 21:39:04 -0700

    wall layer

--- Assets/_Game/Scripts/BecomeLayer.cs
+++ Assets/_Game/Scripts/BecomeLayer.cs
@@ -3,13 +3,11 @@ using UnityEngine.Tilemaps;
 
 public class BecomeLayer : MonoBehaviour
 {
-    public LayerMask mask;
-        
     private void Start()
     {
         foreach (TilemapCollider2D tilemap in GetComponentsInChildren<TilemapCollider2D>())
         {
-            tilemap.gameObject.layer = mask;
+            tilemap.gameObject.layer = LayerMask.NameToLayer("Wall");
         }
     }
 }
