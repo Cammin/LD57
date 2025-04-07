3e3fdfe581e80dc5332554cdc4d9469e4551d242
Cameron <cameo221@gmail.com>
2025-04-07 10:52:07 -0700

    exit door worksproperly

--- Assets/_Game/Scripts/ExitDoor.cs
+++ Assets/_Game/Scripts/ExitDoor.cs
@@ -6,6 +6,8 @@ public class ExitDoor : MonoBehaviour
     public GameObject Door;
     public AudioSource Sound;
 
+    public bool Revealed;
+    
     private void Start()
     {
         Door.SetActive(false);
@@ -15,10 +17,13 @@ public class ExitDoor : MonoBehaviour
     {
         Door.SetActive(true);
         Sound.Play();
+        Revealed = true;
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        if (!Revealed) return;
+        
         if (other.TryGetComponent<Player>(out var player))
         {
             FadeManager.Instance.FadeIn(Color.white, LoadWinScene);
