a7cd27653823ed024259236edf5373a62b1d20e0
Cameron <cameo221@gmail.com>
2025-04-06 21:31:28 -0700

    battery will currently drain when respawning

--- Assets/_Game/Scripts/Checkpoint.cs
+++ Assets/_Game/Scripts/Checkpoint.cs
@@ -1,18 +1,19 @@
 using LDtkUnity;
 using UnityEngine;
 
-public class Checkpoint : MonoBehaviour, ILDtkImportedEntity
+public class Checkpoint : MonoBehaviour, ILDtkImportedEntity, ILDtkImportedFields
 {
     public bool IsStart;
-    
+
     private static string LastCheckpoint;
     public string iid;
+    public bool InvincibleBatteryOnSpawn;
 
     public AudioSource Drink;
     public Animator Anim;
 
     public bool Touched;
-    
+
     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
     public static void ResetCheckpoint()
     {
@@ -21,27 +22,31 @@ public class Checkpoint : MonoBehaviour, ILDtkImportedEntity
 
     private void Start()
     {
+        // put player to spawn, assuming we have no stored location
         if (IsStart && LastCheckpoint == null)
         {
             Player.Instance.transform.position = transform.position;
             return;
         }
-        
+
+        //set player to alternate respawn point
         if (LastCheckpoint == iid)
         {
             Player.Instance.transform.position = transform.position;
+            Player.Instance.IsDrainingFlashlight = !InvincibleBatteryOnSpawn;
+            Destroy(gameObject);
         }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (Touched) return;
-        
+
         if (other.TryGetComponent<Player>(out var player))
         {
             LastCheckpoint = iid;
             Touched = true;
-            
+
             if (!IsStart)
             {
                 Drink.Play();
@@ -60,5 +65,10 @@ public class Checkpoint : MonoBehaviour, ILDtkImportedEntity
     public void OnLDtkImportEntity(EntityInstance entityInstance)
     {
         iid = entityInstance.Iid;
+    }
+
+    public void OnLDtkImportFields(LDtkFields fields)
+    {
+        fields.TryGetBool("InvincibleBatteryOnSpawn", out InvincibleBatteryOnSpawn);
     }
 }
