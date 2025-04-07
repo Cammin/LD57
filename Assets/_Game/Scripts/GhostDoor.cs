b6e5d85829c38557088b01f140686191ad20bb48
Cameron <cameo221@gmail.com>
2025-04-06 18:14:12 -0700

    door FX setup

--- Assets/_Game/Scripts/GhostDoor.cs
+++ Assets/_Game/Scripts/GhostDoor.cs
@@ -10,6 +10,14 @@ public class GhostDoor : MonoBehaviour, ILDtkImportedFields
     public AudioSource Sound;
 
     private bool Destroying;
+    
+    public Collider2D Collider;
+
+    public GameObject Idle1;
+    public GameObject Idle2;
+    
+    public GameObject Open1;
+    public GameObject Open2;
 
     public void OnLDtkImportFields(LDtkFields fields)
     {
@@ -31,6 +39,14 @@ public class GhostDoor : MonoBehaviour, ILDtkImportedFields
             {
                 Destroying = true;
                 Sound.Play();
+                
+                Collider.enabled = false;
+                
+                Idle1.SetActive(false);
+                Idle2.SetActive(false);
+                Open1.SetActive(true);
+                Open2.SetActive(true);
+                
                 Destroy(gameObject, 1f);
             }
         }
